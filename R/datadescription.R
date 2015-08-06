e95f34e0984ba96d5a570fdfd7223253b7b5124c
Witold Wolski <wewolski@gmail.com>
2015-08-06 21:32:36 +0200

    added import from statements

--- R/datadescription.R
+++ R/datadescription.R
@@ -2,7 +2,13 @@
 #'@name quantable
 #'@docType package
 #'@import RColorBrewer scales gplots
+#'@importFrom grDevices dev.off heat.colors
+#'@importFrom graphics abline axis image layout lines pairs plot points text
+#'@importFrom stats cor.test dist hclust mad median p.adjust qqplot runmed t.test
+
 NULL
 # hack to supress _no visible binding for global variable _ warning in R CMD check.
+
+importFrom()
 utils::globalVariables(c("ratio"), add = TRUE)
 print("loading package quantable")
