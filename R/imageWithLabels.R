b8b74b6132c0c011fd1f8d6897da19d9c7902330
Witold Wolski <wewolski@gmail.com>
2015-10-06 13:15:20 +0200

    adding mar parameters to imageWithLabels

--- R/imageWithLabels.R
+++ R/imageWithLabels.R
@@ -8,22 +8,31 @@
 #' @param main main title
 #' @param col color map for matrix
 #' @param digits number of digits on colorscale, default 2
+#' @param marLeft margins of left image see ?par for more detail
+#' @param marRight margins of right image see ?par for more detail
 #' @export
 #' @examples
 #' x = matrix(rnorm(20*30),ncol=20)
 #' rownames(x) <- 1:30
 #' colnames(x) <- letters[1:20]
+#' martmp <- par()$mar
 #' imageWithLabels(x)
+#' imageWithLabels(x,marLeft = c(5,5,2,2),marRight=c(0,0,0,0))
+#' par(mar = martmp)
+#' 
 imageWithLabels = function(x, col.labels=colnames(x), row.labels=rownames(x), cex=1,cex.axis=0.5,main=NULL,
-                           col = heat.colors(12), digits=2)
+                           col = heat.colors(12), digits=2, marLeft=par()$mar, marRight = par()$mar)
 {
-  layout(matrix(data=c(1,2), nrow=1, ncol=2), widths=c(3,1), heights=c(1,1))
 
+  layout(matrix(data=c(1,2), nrow=1, ncol=2), widths=c(3,1), heights=c(1,1))
+  par(mar=marLeft)
   image(x, axes = F, main =main, col=col)
   axis( 2, at=seq(0,1,length=length((col.labels))) , labels=col.labels,cex.axis=cex.axis, las=2, cex=cex )
   axis( 1, at=seq(0,1,length=length((row.labels))) , labels=row.labels,cex.axis=cex.axis, las=2, cex=cex )
 
   colorlevels = seq(min(x,na.rm = TRUE),max(x,na.rm = TRUE),length=length(col))
+  print(marRight)
+  par(mar=marRight)
   image(1, seq(0,1,length=length(colorlevels)),
         matrix(data=colorlevels, nrow=1),
         col=col,xlab="",ylab="",
