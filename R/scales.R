c4084b364d9713e49812bc9d2b3abd81b239c7b7
Witold Wolski <wewolski@gmail.com>
2015-10-06 13:15:20 +0200

    added colorscales for convenience

---
+++ R/scales.R
@@ -0,0 +1,34 @@
+#' create green color scale
+#' @param  length nr of colors
+#' @examples 
+#' 
+#' library(scales)
+#' show_col(getGreensScale(5))
+#' 
+getGreensScale <- function(length = 10){
+  colsGreen <- brewer_pal(palette = "Greens")(8)
+  ygreen<-gradient_n_pal(colsGreen)(seq(0, 1, length.out = length))
+  return(ygreen)
+}
+#' create blue color scale
+#' @param length nr of colors
+#' @examples
+#' 
+#' library(scales)
+#' show_col(getBlueScale(5))
+getBlueScale <- function(length = 10){
+  colsBlue <- brewer_pal(palette = 1)(8)
+  yblue<-gradient_n_pal(colsBlue)(seq(0, 1, length.out = length))
+  return(yblue)
+}
+#' create red color scale
+#' @param length nr of colors
+#' @examples
+#' 
+#' library(scales)
+#' show_col(getRedScale(12))
+getRedScale <- function(length = 10){
+  colsRed <- brewer_pal(palette = 3)(5)
+  yred<-gradient_n_pal(colsRed)(seq(0, 1, length.out = length))
+  return(yred)
+}
--- R/uppertriang.R
+++ R/uppertriang.R
@@ -1,11 +1,12 @@
 #' get values of upper triangle from matrix
 #' @export
 #' @param mat matrix
+#' @param diag default = FALSE
 #' @examples
 #'
 #' t = matrix(1:25,ncol=5)
 #' uppertriang(t)
-uppertriang <- function(mat){
-  res<-mat[upper.tri(mat,diag=FALSE)]
+uppertriang <- function(mat,diag=FALSE){
+  res<-mat[upper.tri(mat,diag=diag)]
   return( c(unlist(res)) )
 }
