c4bc2b76522c09bbaa8d03710eda5fa167142ce0
Witold Wolski <wewolski@gmail.com>
2015-11-05 09:31:45 +0100

    added xlab and ylab to imageWithLabels

--- R/imageWithLabels.R
+++ R/imageWithLabels.R
@@ -10,6 +10,8 @@
 #' @param digits number of digits on colorscale, default 2
 #' @param marLeft margins of left image see ?par for more detail
 #' @param marRight margins of right image see ?par for more detail
+#' @param xlab x label
+#' @param ylab y label
 #' @export
 #' @examples
 #' x = matrix(rnorm(20*30),ncol=20)
@@ -17,16 +19,16 @@
 #' colnames(x) <- letters[1:20]
 #' martmp <- par()$mar
 #' imageWithLabels(x)
-#' imageWithLabels(x,marLeft = c(5,5,2,2),marRight=c(0,0,0,0))
+#' imageWithLabels(x,marLeft = c(5,5,2,2),marRight=c(0,0,0,0),xlab="ttt",ylab="bbb")
 #' par(mar = martmp)
 #' 
 imageWithLabels = function(x, col.labels=colnames(x), row.labels=rownames(x), cex=1,cex.axis=0.5,main=NULL,
-                           col = heat.colors(12), digits=2, marLeft=par()$mar, marRight = par()$mar)
+                           col = heat.colors(12), digits=2, marLeft=par()$mar, marRight = par()$mar, xlab='',ylab='')
 {
 
   layout(matrix(data=c(1,2), nrow=1, ncol=2), widths=c(3,1), heights=c(1,1))
   par(mar=marLeft)
-  image(x, axes = F, main =main, col=col)
+  image(x, axes = F, main =main, col=col,xlab=xlab,ylab=ylab)
   axis( 2, at=seq(0,1,length=length((col.labels))) , labels=col.labels,cex.axis=cex.axis, las=2, cex=cex )
   axis( 1, at=seq(0,1,length=length((row.labels))) , labels=row.labels,cex.axis=cex.axis, las=2, cex=cex )
 
