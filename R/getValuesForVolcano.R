50cbf99cff118857f6131d533aa34f4d0da31ea7
Witold Wolski <wewolski@gmail.com>
2016-01-26 15:40:32 +0100

    added wilkoxonrank test

--- R/getValuesForVolcano.R
+++ R/getValuesForVolcano.R
@@ -1,4 +1,4 @@
-#' running robust scaling of arefw
+#' get p-values of t-test values for volcano
 #' @export
 #' @param x - one data matrix
 #' @param y - second data matrix
@@ -12,7 +12,7 @@
 #' boxplot(t(b[1:20,]))
 #' res <- getValuesForVolcano(a,b)
 #' volcanoplot(res$fchange , res$pval)
-getValuesForVolcano=function(x,y, paired = FALSE){
+getTValuesForVolcano=function(x,y, paired = FALSE){
   stopifnot(nrow(x) == nrow(y))
   pval = rep(NA, nrow(x))
   fchange = rep(NA, nrow(x))
@@ -20,6 +20,33 @@ getValuesForVolcano=function(x,y, paired = FALSE){
     tmp <- t.test(x[i,],y[i,], paired = paired)
     pval[i] <- tmp$p.value
     fchange[i] <-tmp$estimate[1] - tmp$estimate[2]
+  }
+  return(list(pval= p.adjust(pval, method="BH"), fchange=fchange))
+}
+#' get p-values of wilcoxon rank sum test for volcano
+#' @export
+#' @param x - one data matrix
+#' @param y - second data matrix
+#' @param paired a logical indicating whether you want a paired t-test.
+#' @return list with two fields fchange (fold change) and pval
+#' @examples
+#' a <- t(replicate(200,rnorm(20,runif(1,-3,3),1)))
+#' b <- a[1:100,]
+#' a <- a[101:200,]
+#' boxplot(t(a[1:20,]))
+#' boxplot(t(b[1:20,]))
+#' res <- getValuesForVolcano(a,b)
+#' volcanoplot(res$fchange , res$pval)
+getWRValuesForVolcano=function(x,y, paired = FALSE){
+  stopifnot(nrow(x) == nrow(y))
+  pval = rep(NA, nrow(x))
+  fchange = rep(NA, nrow(x))
+  for(i in 1:nrow(x)){
+    xv<-as.numeric(x[i,])
+    yv<-as.numeric(y[i,])
+    tmp <- wilcox.test(xv,yv, paired = paired)
+    pval[i] <- tmp$p.value
+    fchange[i] <- median( xv) - median(yv)
   }
   return(list(pval= p.adjust(pval, method="BH"), fchange=fchange))
 }
