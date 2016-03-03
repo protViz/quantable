58dbeccd45d45f1309f4a2323112cf52a0bc1811
Witold Wolski <wewolski@gmail.com>
2016-03-03 16:45:24 +0100

    added scale and center parameters to scaleByGroup

--- R/scaleByGroup.R
+++ R/scaleByGroup.R
@@ -3,15 +3,22 @@
 #' @param data matrix
 #' @param protGroup names matching rownames of data
 #' @param plot plot diagnostics
+#' @param scale scale default TRUE
+#' @param center center default TRUE
 #' @export
 #' 
-scaleByGroup <- function(data , protGroup, plot=FALSE){
+scaleByGroup <- function(data , protGroup, plot=FALSE, scale=TRUE, center=TRUE){
   reference = data[rownames(data) %in% protGroup,]
   noReference = data[!rownames(data) %in% protGroup,]
-  referenceScaled = robustscale(reference)
-  noReference = sweep(noReference,2,referenceScaled$medians,"-")
-  noReference = sweep(noReference,2,referenceScaled$mads,"/")
-
+  
+  referenceScaled = robustscale(reference,scale=scale, center=center)
+  
+  if(center){
+    noReference = sweep(noReference,2,referenceScaled$medians,"-")
+  }
+  if(scale){
+    noReference = sweep(noReference,2,referenceScaled$mads,"/")
+  }
   if(plot){
     par(mfrow=c(1,2))
     boxplot(noReference,main="noReference",ylim=c(-8,6), pch=".", las=2,cex.axis=0.5)
