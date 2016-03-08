6fb128205a56cc037129fb2944b2509c2df915da
Witold Wolski <wewolski@gmail.com>
2016-03-08 17:13:28 +0100

    adding aggregation function

--- R/sumtop.R
+++ R/sumtop.R
@@ -1,11 +1,19 @@
+#' sums top rows of matrix
+#' uses median of row to determine row order
+#' @export
+#' @param x a matrix
+#' @param top how many to rows to sum (default 3)
+#' @examples
+#' tmp <- rbind(rep(1,times = 4), rep(2,times=4), rep(3,times=4), rep(4,times=4))
+#' res <- sumtop(tmp)
+#' stopifnot(res == c(9,9,9,9))
+#' 
 sumtop <- function( x , top=3 ){
   if(nrow(x) > top){
     topN = min(nrow(x),top)
     medrow <- apply(x, 1 , median)
     ord<-order(medrow, decreasing = TRUE)[1:topN]
-    medrow[ord]
     x<-x[ord,]
-    print(dim(x))
   }
   return(apply(x,2,sum,na.rm=TRUE))
 }
