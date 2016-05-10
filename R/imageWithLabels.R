000e92220d71895e8a687c6cc416a4e6fa2e323e
Witold Wolski <wewolski@gmail.com>
2016-05-10 11:57:41 +0200

    added na.colors to imageWithLabels

--- R/imageWithLabels.R
+++ R/imageWithLabels.R
@@ -1,3 +1,35 @@
+#Copy of http://stackoverflow.com/questions/20977477/how-to-assign-a-specific-color-to-na-in-an-image-plot
+image.nan <- function(z,
+                      col,
+                      zlim=NULL,
+                      na.color='gray',
+                      outside.below.color='pink',
+                      outside.above.color='green',...)
+{
+  if(is.null(zlim)){
+    zlim <- range(z,na.rm=TRUE)
+    print(zlim)
+  }
+  zstep <- (zlim[2] - zlim[1]) / (length(col)-1); # step in the color palette
+  newz.below.outside <- zlim[1] - zstep # new z for values below zlim
+  newz.above.outside <- zlim[2] + zstep # new z for values above zlim
+  newz.na <- zlim[2] + 2 * zstep # new z for NA
+  #print(which(z < zlim[1]))
+  #print(which(z > zlim[2]))
+  
+  z[which(z < zlim[1])] <- newz.below.outside # we affect newz.below.outside
+  z[which(z > zlim[2])] <- newz.above.outside # we affect newz.above.outside
+  z[which(is.na(z))] <- newz.na # same for newz.na
+  cat("range z", range(z), "\n" )
+  
+  zlim[1] <- zlim[1] - zstep # extend lower limit to include below value
+  zlim[2] <- zlim[2] + 2 * zstep # extend top limit to include the two new values above and na
+  cat('range zlim',zlim,'\n')
+  col <- c(outside.below.color, col, outside.above.color, na.color) # we construct the new color range by including: na.color and na.outside
+  print(length(col))
+  image(z=z, zlim=zlim, col=col, ...) # we finally call image(...)
+}
+
 #' image plot with labels
 #'
 #' @param x matrix
@@ -16,16 +48,26 @@
 #' x = matrix(rnorm(20*30),ncol=20)
 #' rownames(x) <- 1:30
 #' colnames(x) <- letters[1:20]
-#' imageWithLabelsNoLayout(x)
+#' imageWithLabelsNoLayout(x,col = heat.colors(13))
+#' imageWithLabelsNoLayout(x,col = heat.colors(12))
+#' range(x)
 #' imageWithLabelsNoLayout(x,xlab="ttt",ylab="bbb")
 #' 
-imageWithLabelsNoLayout = function(x, col.labels=colnames(x), row.labels=rownames(x), cex=1,cex.axis=0.5,main=NULL,
-                           col = heat.colors(12), digits=2, xlab='',ylab='', zlim=NULL)
-{
+imageWithLabelsNoLayout = function(x,
+                                   col.labels=colnames(x),
+                                   row.labels=rownames(x),
+                                   cex=1,
+                                   cex.axis=0.5,main=NULL,
+                                   col = heat.colors(12),
+                                   digits=2,
+                                   xlab='',
+                                   ylab='',
+                                   zlim=NULL,
+                                   na.color='gray'){
   if(!is.null(zlim)){
-    image(x, axes = F, main =main, col=col,xlab=xlab,ylab=ylab,zlim=zlim)
+    image.nan(x, axes = F, main =main, col=col,xlab=xlab,ylab=ylab,zlim=zlim)
   }else{
-    image(x, axes = F, main =main, col=col,xlab=xlab,ylab=ylab)
+    image.nan(x, axes = F, main =main, col=col,xlab=xlab,ylab=ylab)
   }
   
   axis( 2, at=seq(0,1,length=length((col.labels))) , labels=col.labels,cex.axis=cex.axis, las=2, cex=cex )
