41a0723d911178ef7e6e4484da54a3b0443241cb
Witold Wolski <wewolski@gmail.com>
2016-07-15 15:31:05 +0200

    nop

--- R/write.vector.R
+++ R/write.vector.R
@@ -1,8 +1,10 @@
-#' write vector into table (usefull for exporting protein id's)
+#' write vectors as single column table
+#' (usefull for exporting i.e. protein id's)
 #' uses write.table
 #' @param x vector to write
 #' @param file file to write to
 #' @export
+#' 
 write.vector <- function(x,file){
   utils::write.table(x,file=file, quote=FALSE,row.names = FALSE,sep="\t",col.names = FALSE)  
 }
