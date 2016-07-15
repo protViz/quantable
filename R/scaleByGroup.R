8a4488a992beae5c35920ff218a5c1c2af362080
Witold Wolski <wewolski@gmail.com>
2016-07-15 14:53:41 +0200

    fixing note in check

--- R/scaleByGroup.R
+++ R/scaleByGroup.R
@@ -23,7 +23,7 @@ scaleByGroup <- function(data , protGroup, plot=FALSE, scale=TRUE, center=TRUE){
     graphics::par(mfrow=c(1,2))
     graphics::boxplot(noReference,main="noReference",ylim=c(-8,6), pch=".", las=2,cex.axis=0.5)
     abline(h=0,col=2)
-    graphics::graphics::boxplot(referenceScaled$data,main="reference",ylim=c(-8,6),las=2,pch=".",cex.axis=0.5)
+    graphics::boxplot(referenceScaled$data,main="reference",ylim=c(-8,6),las=2,pch=".",cex.axis=0.5)
     abline(h=0,col=2)
   }
   return(list(reference = referenceScaled$data, noReference = noReference))
