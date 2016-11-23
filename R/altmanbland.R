a6970dd08a941e72a63be543a8bd4c981b8510b3
Witold Wolski <wewolski@gmail.com>
2016-11-23 11:09:18 +0100

    cleanup.

--- R/altmanbland.R
+++ R/altmanbland.R
@@ -19,10 +19,8 @@ altmanbland = function(x,y,main="",pch=".",log=""){
   x <- x[!nas]
   y <- y[!nas]
   
-  idx<-apply(cbind(x,y),1,function(x){sum(x==0)==0})
   mean  = (x+y)/2
   absdiff = abs( x-y )
-  #scatter.smooth(mean[idx],absdiff[idx],span=1/3,col=2,pch=".",cex=2,xlab="(y+x)/2",ylab="abs(x-y)",main=main,log="xy")
   plot(mean,absdiff,log=log,xlab="(y+x)/2",ylab="abs(x-y)",pch="x",cex=0.5,main=main)
   lines(lowess(mean,absdiff),col=2,lwd=2)
 }
