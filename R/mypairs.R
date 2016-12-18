ef652df93547c3ca1ab1269cfee7fa7e7bcf8599
Witold Wolski <wewolski@gmail.com>
2016-12-18 13:37:38 +0100

    adding param pch to mypairs

--- R/mypairs.R
+++ R/mypairs.R
@@ -2,15 +2,16 @@
 #' @param dataframe data matrix or data.frame as normally passed to pairs
 #' @param ... params usually passed to pairs
 #' @param legend  add legend to plots
+#' @param pch point type default "."
 #' @export
 #' @examples
 #' tmp = matrix(rep((1:100),times = 4) + rnorm(100*4,0,3),ncol=4)
 #' mypairs(tmp,log="xy",main="small data")
 #' mypairs(tmp,log="xy",main="small data", legend=TRUE)
 #' @seealso also \code{\link{pairs}}
-mypairs = function(dataframe,legend=FALSE,...){
+mypairs = function(dataframe,legend=FALSE,pch=".",...){
   pairs(dataframe, panel = function(x,y){
-    graphics::points(x, y, pch=".")
+    graphics::points(x, y, pch=pch)
     graphics::abline(a=0,b=1,v=0,h=0,col=2)
     if(legend){
       cR2 <- stats::cor(x,y, use="pairwise.complete.obs")^2
