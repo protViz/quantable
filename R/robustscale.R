d8c832818d8a4084be6950a6a15622446d48d95a
Witold Wolski <wewolski@gmail.com>
2017-04-21 11:15:49 +0200

    adjust scale with preserve scale

--- R/robustscale.R
+++ R/robustscale.R
@@ -6,7 +6,9 @@
 #' @param dim - should rows (1) or columns (2:default) be scaled
 #' @param center - subract median (default:TRUE)
 #' @param scale - scale by mad  (default:FALSE)
+#' @param perserveScale - 
 #' @examples
+#' library(quantable)
 #' tmp = matrix(rep((1:100),times = 4) + rnorm(100*4,0,3),ncol=4)
 #' mean = c(20,30,10,40)
 #' sd = c(4,3,4,5)
@@ -15,7 +17,7 @@
 #' boxplot(tmp)
 #' tmp = robustscale(tmp)
 #' boxplot(tmp$data)
-robustscale <- function(data, dim=2, center=TRUE, scale=TRUE){
+robustscale <- function(data, dim=2, center=TRUE, scale=TRUE, preserveScale = TRUE){
   medians = NULL
   if(center){
     medians <- apply(data,dim,median,na.rm=TRUE)
@@ -24,6 +26,9 @@ robustscale <- function(data, dim=2, center=TRUE, scale=TRUE){
   mads=NULL
   if(scale){
     mads <- apply(data,dim, mad,na.rm =TRUE)
+    if(preserveScale){
+      mads <- mads/mean(mads)
+    }
     data = (sweep(data,dim,mads,"/"))
   }
   return(list(data=data,medians=medians,mads=mads))
