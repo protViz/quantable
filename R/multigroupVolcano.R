29ea9a0fe0564a5b0cf38ad8d1b15e76e58ee614
Witold Wolski <wewolski@gmail.com>
2017-06-27 18:23:21 +0200

    incremental improvements to mypairs and multigroupVolcano

--- R/multigroupVolcano.R
+++ R/multigroupVolcano.R
@@ -6,26 +6,27 @@
 #' @param xintercept fc thresholds
 #' @param pvalue pvalue threshold
 #' @param label column containing labels
-#' 
+#' @importFrom ggrepel geom_text_repel
 #' @export
 #' 
-multigroupVolcano <- function(misspX,
-                        effect = "fc",
-                        type = "p.adjust",
-                        Condition = "condition",
-                        xintercept=c(-2,2),
-                        pvalue=0.05, label="row.names") {
+multigroupVolcano <- function(misspX,colour,
+                              effect = "fc",
+                              type = "p.adjust",
+                              Condition = "condition",
+                              xintercept=c(-2,2),
+                              pvalue=0.05, label="row.names" ) {
   col = paste("-log10(", type , ")" , sep="")
   message(col)
-  p <- ggplot( misspX, aes_string(x = effect , y = col  ))  + geom_point()
+  p <- ggplot( misspX, aes_string(x = effect , y = col  ))  + geom_point(col=colour, alpha=0.5)
   p <- p + facet_wrap(as.formula(paste("~",Condition))) + labs(y = col)
   p <- p + geom_abline(data = cc, aes(slope = sl, intercept = -log10(p),colour = Area)) + 
     geom_vline(xintercept = xintercept,linetype = "dashed", colour = "red")
-  if(0){
+  if(1){
+    message("test")
     effectX <-misspX[,effect]
     typeX<-misspX[,type]
     subsetData <- subset(misspX, (effectX < xintercept[1] | xintercept[2] < effectX) & typeX < pvalue )
-    p <- p + geom_text(data=subsetData, aes_string(effect , col , label=label))
+    p <- p + geom_text_repel(data=subsetData, aes_string(effect , col , label=label))
   }
   return(p)
 }
--- R/mypairs.R
+++ R/mypairs.R
@@ -20,6 +20,37 @@ mypairs = function(dataframe,legend=FALSE,pch=".",...){
     , lower.panel=NULL,...
   )
 }
+panel.hist <- function(x, ...)
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(usr[1:2], 0, 1.5) )
+  h <- hist(x, plot = FALSE)
+  breaks <- h$breaks; nB <- length(breaks)
+  y <- h$counts; y <- y/max(y)
+  rect(breaks[-nB], 0, breaks[-1], y,  ...)
+}
+
+panel.cor <- function(x, y, digits = 2, cex.cor, ...)
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  # correlation coefficient
+  r <- cor(x, y)
+  txt <- format(c(r, 0.123456789), digits = digits)[1]
+  txt <- paste("r= ", txt, sep = "")
+  text(0.5, 0.7, txt)
+  
+  txt <- format(c(r^2, 0.123456789), digits = digits)[1]
+  txt <- bquote(R^{2} ~"=" ~ .(txt))
+  text(0.5, 0.5, txt)
+  
+  # p-value calculation
+  p <- cor.test(x, y)$p.value
+  txt2 <- format(c(p, 0.123456789), digits = digits)[1]
+  txt2 <- paste("p= ", txt2, sep = "")
+  if(p<0.01) txt2 <- paste("p= ", "<0.01", sep = "")
+  text(0.5, 0.3, txt2)
+}
 
 #' smoothScatter pairs
 #' @param dataframe data matrix or data.frame as normally passed to pairs
@@ -30,17 +61,17 @@ mypairs = function(dataframe,legend=FALSE,pch=".",...){
 #' @examples
 #' tmp = matrix(rep((1:100),times = 4) + rnorm(100*4,0,3),ncol=4)
 #' mypairsSmooth(tmp,main="small data", legend=TRUE)
-#' mypairsSmooth(tmp,log="xy",main="small data")
+#' mypairsSmooth(tmp,main="small data", diag.panel=panel.hist)
 #' mypairsSmooth(tmp,log="xy",main="small data", legend=TRUE)
 #' @seealso also \code{\link{pairs}}
 mypairsSmooth = function(dataframe,legend=FALSE,...){
-  pairs(dataframe, panel = function(x,y){
+  pairs(dataframe, upper.panel = function(x,y){
     graphics::smoothScatter(x, y, add=TRUE)
     graphics::abline(a=0,b=1,v=0,h=0,col=2)
     if(legend){
       cR2 <- stats::cor(x,y, use="pairwise.complete.obs")^2
       graphics::legend("topleft", legend=paste("R^2=", round(cR2, digits=2) , sep=""),text.col=3)}
   }
-  , lower.panel=NULL,...
+  , lower.panel=panel.cor, ...
   )
 }
