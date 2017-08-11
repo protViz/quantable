5551536c04a8db54526ea31167bbab8ed2ec0cdd
Witold Wolski <wewolski@gmail.com>
2017-08-11 11:59:02 +0200

    upping travis file.

--- R/datadescription.R
+++ R/datadescription.R
@@ -1,7 +1,7 @@
 #'Streamline descriptive analysis of quantitative data matrices
 #'@name quantable
 #'@docType package
-#'@import RColorBrewer gplots e1071
+#'@import RColorBrewer e1071
 #'@importFrom grDevices dev.off heat.colors
 #'@importFrom graphics abline axis image layout lines pairs plot points text
 #'@importFrom stats cor.test dist hclust mad median p.adjust qqplot runmed t.test
