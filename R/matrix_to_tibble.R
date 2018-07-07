710e71d7fe45de9b01a177d36820efef4538099e
Witold Wolski <wewolski@gmail.com>
2018-07-07 21:02:07 +0200

    give name to row.names column

--- R/matrix_to_tibble.R
+++ R/matrix_to_tibble.R
@@ -1,21 +1,26 @@
-#' Matrix to tibble (taken from tidyquant)
+#' matrix or data.frame to tibble (taken from tidyquant)
 #' 
 #' @param x a matrix
-#' @param preserve_row_names should rownames be kept (default TRUE)
+#' @param preserve_row_names give name to rownames column, if NULL discard rownames
 #' @param ... further parameters passed to as_tibble
 #' @importFrom dplyr bind_cols
 #' @importFrom tibble tibble as_tibble
 #' @export
+#' @examples 
+#' x <- matrix(rnorm(20), ncol=4)
+#' rownames(x) <- LETTERS[1:nrow(x)]
+#' matrix_to_tibble(x)
+#' !(is.matrix(x) || is.data.frame(x))
 #' 
-matrix_to_tibble <- function(x, preserve_row_names = TRUE,...)
+matrix_to_tibble <- function(x, preserve_row_names = "row.names",...)
 {
-  if (!is.matrix(x)) stop("Error: `x` is not a matrix object.")
-  if (preserve_row_names == TRUE) {
+  if (!(is.matrix(x) || is.data.frame(x))) stop("Error: `x` is not a matrix or data.frame object.")
+  if (!is.null(preserve_row_names)) {
     row.names <- rownames(x)
     if (!is.null(row.names)  ) {
       #&& !identical(row.names, 1:nrow(x) %>% as.character())
       dplyr::bind_cols(
-        tibble::tibble(row.names),
+        tibble::tibble(!! preserve_row_names := row.names),
         tibble::as_tibble(x, ...)
       )
       
@@ -32,3 +37,4 @@ matrix_to_tibble <- function(x, preserve_row_names = TRUE,...)
     
   }
 }
+
