58919823aa84c67a1d7b595459e0f8f7e9be402b
LucasKookUZH <lucasheinrich.kook@uzh.ch>
2018-11-26 14:37:33 +0100

    Updating ifelse statement and documentation

--- R/simpleheatmap3.R
+++ R/simpleheatmap3.R
@@ -16,25 +16,46 @@
 #' @param suppressColSideCols Option to suppress color side labelling per default
 #' @param ... other parameters to heatmap3
 #' @examples
-#' df <- matrix(rnorm(150), ncol = 10) # 15x10 matrix
+#' df <- matrix(rnorm(150), ncol = 10) # 15x10 matrix without NAs
+#' 
 #' clust <- simpleheatmap3(pln = df,
-#'  main = "",
-#'  distf = dist,
-#'  hclustf = hclust,
-#'  labRow = "",
-#'  plot = FALSE,
-#'  nrOfClustersRow = 3) 
+#'                         main = "",
+#'                         distf = dist,
+#'                         hclustf = hclust,
+#'                         labRow = "",
+#'                         plot = FALSE,
+#'                         nrOfClustersRow = 3,
+#'                         suppressColSideCols = TRUE) 
 #' # plot = F will return a list containing two data.frames 
 #' # with the specified number of distinct clusters in nrOfClustersRow
 #' # and nrOfClustersCol
+#' 
 #' hmp3 <- simpleheatmap3(pln = df,
-#'  main = "",
-#'  distf = dist,
-#'  hclustf = hclust,
-#'  labRow = "",
-#'  plot = FALSE,
-#'  nrOfClustersRow = 3)  
-
+#'                        main = "",
+#'                        distf = dist,
+#'                        hclustf = hclust,
+#'                        labRow = "",
+#'                        plot = TRUE,
+#'                        nrOfClustersRow = 3, 
+#'                        nrOfClustersCol = 3, 
+#'                        suppressColSideCols = FALSE)
+#' # plot = TRUE will result in the same list and additionally plot the heatmap. 
+#' suppressColSideCols = FALSE #' will leave the heatmap with ColSideColors
+#' 
+#' df[3,1:7] <- NA # Seed some missing values in line 3
+#' 
+#' hmp3_withNAs <- simpleheatmap3(pln = df,
+#'                        main = "",
+#'                        distf = dist,
+#'                        hclustf = hclust,
+#'                        labRow = "",
+#'                        plot = TRUE,
+#'                        nrOfClustersRow = 3, 
+#'                        nrOfClustersCol = 3, 
+#'                        suppressColSideCols = FALSE)
+#' # Since line 3 contained more than ncol(df)/2 missing values 
+#' it is removed from the clustering.
+#' 
 
 simpleheatmap3 <- function(pln,
                            main="",
@@ -49,31 +70,10 @@ simpleheatmap3 <- function(pln,
                            nrOfClustersRow = 3,
                            suppressColSideCols = FALSE, ...)
 {
-  if(plot&!suppressColSideCols) {
+  pln <- as.matrix(pln[rowNAs(pln) < (ncol(pln)/2),])
+  if(plot) {
     tmp0 <- cutree(hclustf(distf(t(as.matrix(pln)))), nrOfClustersCol)
     colsidecolors <- rainbow(nrOfClustersCol)[tmp0]
-    tmp <- heatmap3::heatmap3(as.matrix(pln), scale=scale, col=palette,
-              labRow=labRow,
-              labCol = labCol,
-              cexRow=0.1 + 1/log10(dim(pln)[1]),
-              cexCol=0.1 + 1/log10(dim(pln)[2]),
-              distfun=distf,hclustfun=hclustf,
-              margins=margins,main=main,
-              keep.dendro = TRUE,
-              ColSideColors = colsidecolors,
-              ColSideLabs = "",
-              RowSideLabs = "",
-              ...=...)
-    clusterIDsRow <- cutree(as.hclust(tmp$Rowv), nrOfClustersRow)
-    clusterIDsCol <- cutree(as.hclust(tmp$Colv), nrOfClustersCol)
-    return(list(Row = data.frame(rowID = labRow,
-                                 clusterID = clusterIDsRow,
-                                 stringsAsFactors = F),
-                Col = data.frame(colID = labCol,
-                                 clusterID = clusterIDsCol,
-                                 stringsAsFactors = F)))
-  }
-  else if(plot&suppressColSideCols) {
     tmp <- heatmap3::heatmap3(as.matrix(pln), scale=scale, col=palette,
                               labRow=labRow,
                               labCol = labCol,
@@ -84,6 +84,7 @@ simpleheatmap3 <- function(pln,
                               keep.dendro = TRUE,
                               ColSideLabs = "",
                               RowSideLabs = "",
+                              ColSideColors = get(ifelse(suppressColSideCols, NULL, "colsidecolors")),
                               ...=...)
     clusterIDsRow <- cutree(as.hclust(tmp$Rowv), nrOfClustersRow)
     clusterIDsCol <- cutree(as.hclust(tmp$Colv), nrOfClustersCol)
